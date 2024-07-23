c77b12efecfb078cad584220c0ea5df7b43aadd9
Cameron Morris <cameronjmorris87@gmail.com>
2024-07-23 17:53:52 +1200

    removing [] properly

--- src/Parser.cs
+++ src/Parser.cs
@@ -12,17 +12,20 @@ public class Parser(){
 
         HtmlNodeCollection lyricNodes = htmlSnippet.DocumentNode.SelectNodes("//div[@data-lyrics-container='true']");
         if(lyricNodes != null){
-            var italicNodes = lyricNodes.Descendants("i").ToList();
-            foreach (var node in italicNodes){
-                node.Remove();
-            }
             foreach (HtmlNode node in lyricNodes.Descendants()){        
-                if(node.NodeType == HtmlNodeType.Text && !node.InnerText.Contains('[') && !node.InnerText.Contains(']')){
-                    foreach (string word in node.InnerText.Split(' ', '-')){ 
-                        string decodedWord = System.Net.WebUtility.HtmlDecode(word).Trim();
-                        string cleanedWord = Regex.Replace(decodedWord, "[^a-zA-Z0-9]+", "");
-                        if(cleanedWord != ""){
-                            lyrics.Add(cleanedWord);
+                if(node.NodeType == HtmlNodeType.Text){
+                    bool inQuotes = false;
+                    foreach (string word in node.InnerText.Split(' ', '-')){
+                        if(word.Contains('[')){
+                            inQuotes = true;
+                        }else if (inQuotes && word.Contains(']')){
+                            inQuotes = false;
+                        }else if (!inQuotes){
+                            string decodedWord = System.Net.WebUtility.HtmlDecode(word).Trim();
+                            string cleanedWord = Regex.Replace(decodedWord, "[^a-zA-Z0-9]+", "");
+                            if(cleanedWord != ""){
+                                lyrics.Add(cleanedWord);
+                            }
                         }
                     }
                 }         
